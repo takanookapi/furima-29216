d8fd6d2c3a835e64678d7269cc395ee7d0bda25d
takanookapi <takdanielu@yahoo.co.jp>
2020-08-28 16:38:42 +0900

    Rubocop＋バリデーション＋新規登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,30 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+
+
+      with_options presence: true do
+        validates :name
+        validates :email, uniqueness: true,
+                          format: { with: /\A([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+\z/i}
+        
+        validates :password, confirmation: true,
+                              length: { minimum: 5 },
+                              format: { with: /\A[a-z\d]+\z/i}                   
+      end
+      
+      with_options presence: true, format: { with: /\A[ぁ-んァ-ヶー一-龠]+\z/i} do
+        validates :first_name
+        validates :last_name
+      end
+
+      with_options presence: true, format: { with: /\A[ア-ン゛゜ァ-ォャ-ョー「」、]+\z/i} do
+        validates :first_name_kana
+        validates :last_name_kana
+      end
+
+      with_options presence: true do
+        validates :date
+      end
 end
--- db/migrate/20200827134029_devise_create_users.rb
+++ db/migrate/20200827134029_devise_create_users.rb
@@ -4,13 +4,13 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :name,               null: false  
-      t.string :password,           null: false  
-      t.string :first_name,         null: false                    
-      t.string :last_name,          null: false                    
-      t.string :first_name_kana,    null: false                    
-      t.date :date,                 null: false  
-      t.string :email,              null: false, default: ""
+      t.string :name,               null: false
+      t.string :first_name,         null: false
+      t.string :last_name,          null: false
+      t.string :first_name_kana,    null: false
+      t.string :last_name_kana,     null: false
+      t.date   :date,               null: false
+      t.string :email,              null: false
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
